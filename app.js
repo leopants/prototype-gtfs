447742f8ec597c7d9abf8ecd54ec2a55c154c253
leopants <leopanta99@gmail.com>
2020-03-13 16:40:21 -0400

    removed cors

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function urlCreator(otpHost, reqBody) {
     startTime = reqBody.startTime;
     startDate = reqBody.startDate;
     arriveBy = reqBody.arriveBy;
-    return url = otpHost + '/otp/routers/default/plan?fromPlace=' + fromPlace + '&toPlace=' + toPlace + '&time=' + startTime + '&date=' + startDate + '&mode=TRANSIT,WALK&maxWalkDistance=500&arriveBy=' + arriveBy;
+    return url = otpHost + '/otp/routers/default/plan?fromPlace=' + fromPlace + '&toPlace=' + toPlace + '&time=' + startTime + '&date=' + startDate + '&mode=TRANSIT,WALK&maxWalkDistance=5000&arriveBy=' + arriveBy;
 }
 
 //Function that will parse the api call and return the important stuff
